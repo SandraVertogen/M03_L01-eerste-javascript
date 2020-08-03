f64b1d79df1f0023e9fb6038161699810a542803
SandraVertogen <65301809+SandraVertogen@users.noreply.github.com>
2020-08-03 11:17:02 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log("Hello Winc Academy");
+
+let name = "Sandra";
+console.log(name);
+
+// Je kunt geen variabele maken met if, omdat "if" een gereserveerd woord is
+
+const getal1 = 4;
+const getal2 = 4;
+const string1 = "4";
+const string2 = "4";
+
+console.log(getal1 + getal2);  // Je kunt 2 nummers optellen
+console.log(string1 + string2); // Je kunt 2 strings niet wiskundig  optellen, ze worden 'aan elkaar vast geplakt'
+
+console.log(getal1 + getal2);
+console.log(getal1 - getal2);
+console.log(getal1 * getal2);
+console.log(getal1 / getal2);
+console.log(getal1 % getal2);
+
+let age1 = 35;
+console.log(typeof (age1));
+
+let age2 = "vijfendertig";
+console.log(typeof (age2));
